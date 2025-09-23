95a5dd676cd2089f5866d221a05ab27cb1a03568
Frengocode <wendypython17@gmail.com>
2025-09-23 13:28:28 +0300

    ADDED AND FIXED ###

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from typing import Any
 @pytest.mark.asyncio
 async def test_get_hello() -> None:
     async with aiohttp.ClientSession() as client:
-        response = await client.get("http://localhost:8000/hello/")
+        response = await client.get("http://app:8000/hello/")
         assert response.status == 200
 
         data: dict[str, Any] = await response.json()
